f798c0a1c52e2a903be991f66f0957f595f26724
Julie of the Wolves <juliana.novic@gmail.com>
2018-10-31 19:12:52 -0400

    table appends

--- Aliens_R_Real/static/js/app.js
+++ Aliens_R_Real/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var submit = d3.select("#filter-btn");
+var table = d3.select("#ufo-table");
+var tablebody = table.select("tbody");
+
+console.log(Object.entries(tableData));
+tableData.forEach((user) => {
+    var tablerow = tablebody.append("tr");
+    Object.entries(user).forEach(([key,value]) => {
+        var tabledivision = tablerow.append("td");
+    tabledivision.text(value);})  
+});
+
+
+

0ab642763bcb5254a7255d71dff703a71ad35dcc
Julie of the Wolves <juliana.novic@gmail.com>
2018-11-08 18:27:32 -0500

    function one attempted

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -4,26 +4,48 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-
+  var belly = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-
+    belly.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(sample).forEach(([key,value]) =>{
+      var bellybutton = belly.append("div");
+      bellybutton.text(key + ":" + value);
+    })
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 }
 
+
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+url = /metadata/<sample>
+d3.json(url).then(buildCharts(sample_metadata));
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  var hovertext = sample_metadata.otu_ids.slice(0, 11);
+  var sample_values = sample_metatadata.sample_values.slice(0,11);
+  var labels = sample_metadata.otu_labels.slice(0,11);
+  var trace1 = {
+    labels:labels,
+   values:sample_values ,
+   hovertext: hovertext,
+   type: 'pie'
+ };
+
+ var data = [trace1];
+
+
+ Plotly.newPlot("pie", data, );
+
+    
 }
 
 function init() {
